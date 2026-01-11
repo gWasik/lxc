f97f1c88ea7069ab930548a916d0a1e21d75f641
gWasik <arwasik+github@gmail.com>
2026-01-11 13:43:00 +0300

    -ssh

--- update-owrt.sh
+++ update-owrt.sh
@@ -12,6 +12,6 @@ mv /etc/banner /etc/banner.awasiksave
 #add ssh key
 "wget" "https://raw.githubusercontent.com/gWasik/lxc/refs/heads/main/.ssh/authorized_keys" "-O" "/root/.ssh/authorized_keys"
 #
-sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
-sudo sed -i 's/#ChallengeResponseAuthentication yes/ChallengeResponseAuthentication no/' /etc/ssh/sshd_config
-systemctl restart sshd
+#sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
+#sudo sed -i 's/#ChallengeResponseAuthentication yes/ChallengeResponseAuthentication no/' /etc/ssh/sshd_config
+#systemctl restart sshd
