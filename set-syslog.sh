dae01eb371d37c12e8eb8515664aa1cc44142831
gWasik <arwasik+github@gmail.com>
2026-05-23 12:07:47 +0300

    оптимизация

--- set-syslog.sh
+++ set-syslog.sh
@@ -6,6 +6,9 @@ apt upgrade
 apt install -y htop btop net-tools sudo iftop iperf3 mtr mc atop
 apt autoremove -y
 
+#ufw
+ufw logging off
+
 #add rsyslog 
 "wget" "https://raw.githubusercontent.com/gWasik/lxc/refs/heads/main/etc/rsyslog.d/remote.conf" "-O" "/etc/rsyslog.d/remote.conf"
 apt-get install rsyslog -y
