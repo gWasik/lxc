efd712ad6a20efbbbd6005f91165ced28775fb7c
gWasik <arwasik+github@gmail.com>
2026-05-23 13:04:49 +0300

    sudo

--- set-syslog.sh
+++ set-syslog.sh
@@ -1,31 +1,29 @@
 #!/bin/sh
 
 # add packets
-apt update
-apt upgrade
-apt install -y htop btop net-tools sudo iftop iperf3 mtr mc atop
-apt autoremove -y
+sudo apt update
+sudo apt upgrade
+sudo apt install -y htop btop net-tools sudo iftop iperf3 mtr mc atop sudo
+sudo apt autoremove -y
 
 #ufw
-ufw logging off
+sudo ufw logging off
+sudo ufw allow 11111/tcp
 
 #add rsyslog 
 "wget" "https://raw.githubusercontent.com/gWasik/lxc/refs/heads/main/etc/rsyslog.d/remote.conf" "-O" "/etc/rsyslog.d/remote.conf"
-apt-get install rsyslog -y
+sudo apt-get install rsyslog -y
 rsyslogd -N1
-systemctl restart rsyslog
+sudo systemctl restart rsyslog
 logger "message"
 
 "wget" "https://raw.githubusercontent.com/gWasik/lxc/refs/heads/main/etc/docker/daemon.json" "-O" "/etc/docker/daemon.json"
-systemctl stop docker.service
-systemctl reset-failed docker.service docker.socket
-systemctl start docker.service
+sudo systemctl stop docker.service
+sudo systemctl reset-failed docker.service docker.socket
+sudo systemctl start docker.service
 
 #установка временной зоны MSK
 sudo timedatectl set-timezone Europe/Moscow
-
-#ufw
-ufw allow 11111/tcp
 
 #ssh
 sudo sed -i -E 's/^#?Port 22/Port 11111/' /etc/ssh/sshd_config
